6d18076befcd86e4c7dcf5b98e3d83fd2e80fb4a
jkapp610 <jkapp@umich.edu>
2021-03-25 23:42:31 -0400

    First commit

---
+++ app/src/main/java/com/example/sensor/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.sensor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv1=null;
+    private SensorManager mSensorManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv1 = (TextView) findViewById(R.id.textView2);
+        tv1.setVisibility(View.GONE);
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> mList= mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        tv1.setMovementMethod(new ScrollingMovementMethod());
+        for (int i = 1; i < mList.size(); i++) {
+
+            tv1.setVisibility(View.VISIBLE);
+            tv1.append("\n" + mList.get(i).getName() + "\n" + mList.get(i).getVendor() + "\n" + mList.get(i).getVersion());
+        }
+    }
+}
